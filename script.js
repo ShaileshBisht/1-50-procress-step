3098267a42e9cf5c8bed1c9519d20da25b1afc21
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-14 22:44:29 +0530

    add eventListener to next button

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ const progress = document.getElementById("progess");
 const prev = document.getElementById("prev");
 const next = document.getElementById("next");
 const circles = document.querySelectorAll(".circle");
+
+let currentActive = 1;
+
+next.addEventListener("click", () => {
+  currentActive++;
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+});
