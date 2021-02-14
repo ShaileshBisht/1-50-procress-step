de6d75f6327a97c294db847c9194b81d4a4da3df
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-14 22:42:18 +0530

    USE document to get different dom element

---
+++ script.js
@@ -0,0 +1,4 @@
+const progress = document.getElementById("progess");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
